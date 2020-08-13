da30bc0019e8069bdc8a9a59bc9491796a4ab7ce
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-12 22:13:07 -0500

    feat(info): adds basic tablayout structure

--- app/src/main/java/net/lucasdesouza/countmybreaths/ui/info/InfoFragment.kt
+++ app/src/main/java/net/lucasdesouza/countmybreaths/ui/info/InfoFragment.kt
@@ -2,26 +2,38 @@ package net.lucasdesouza.countmybreaths.ui.info
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_info.*
 import net.lucasdesouza.countmybreaths.R
-import net.lucasdesouza.countmybreaths.data.BreathRecordContract
 
 class InfoFragment : Fragment(R.layout.fragment_info) {
 
-    private lateinit var dbHelper: BreathRecordContract.BreathRecordDbHelper
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dbHelper = BreathRecordContract.getHelper(this.requireContext())
-        deleteAllDataButton.setOnClickListener {
-            BreathRecordContract.deleteAll(dbHelper)
-            BreathRecordContract.fetchAll(dbHelper)
-        }
+
+        info_tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                help_page.visibility = View.GONE
+                pet_health_page.visibility = View.GONE
+                about_page.visibility = View.GONE
+                when(tab) {
+                    info_tabs.getTabAt(0) -> help_page.visibility = View.VISIBLE
+                    info_tabs.getTabAt(1) -> pet_health_page.visibility = View.VISIBLE
+                    info_tabs.getTabAt(2) -> about_page.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                print(tab.toString())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                print(tab.toString())
+            }
+        })
     }
 
-    override fun onDestroy() {
-        BreathRecordContract.helperClose(dbHelper)
-        super.onDestroy()
-    }
 }
